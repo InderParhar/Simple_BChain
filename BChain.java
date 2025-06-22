5d72708d3916b9975485635b268de49f9cca65d0
caslox <inderparhar1997@gmail.com>
2025-06-22 11:21:16 -0400

    Created wallets and transaction

--- BChain.java
+++ BChain.java
@@ -2,10 +2,18 @@ import java.util.ArrayList;
 
 import com.google.gson.GsonBuilder;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+
 public class BChain {
 
     public static ArrayList<Block> blockchain =  new ArrayList<Block>();
     public static int difficulty = 5;
+
+    public static Wallet wallet_a;
+    public static Wallet wallet_b; 
 
     public static Boolean isChainValid(){   
         Block current_Block;
@@ -27,29 +35,40 @@ public class BChain {
         }
         return true;   
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidAlgorithmParameterException {
         
-        Block firstBlock = new Block("First Block", "0");
-        blockchain.add(firstBlock);
-        System.out.println("Trying to mine 1");
-        blockchain.get(0).mineBlock(difficulty);
+        // Block firstBlock = new Block("First Block", "0");
+        // blockchain.add(firstBlock);
+        // System.out.println("Trying to mine 1");
+        // blockchain.get(0).mineBlock(difficulty);
 
-        Block SecondBlock = new Block("Second Block", firstBlock.hash);
-        blockchain.add(SecondBlock);
-        System.out.println("Trying to mine 2");
-        blockchain.get(1).mineBlock(difficulty);
+        // Block SecondBlock = new Block("Second Block", firstBlock.hash);
+        // blockchain.add(SecondBlock);
+        // System.out.println("Trying to mine 2");
+        // blockchain.get(1).mineBlock(difficulty);
 
-        Block ThridBlock = new Block("Third Block", SecondBlock.hash);
-        blockchain.add(ThridBlock);
-        System.out.println("Trying to mine 3");
-        blockchain.get(2).mineBlock(difficulty);
+        // Block ThridBlock = new Block("Third Block", SecondBlock.hash);
+        // blockchain.add(ThridBlock);
+        // System.out.println("Trying to mine 3");
+        // blockchain.get(2).mineBlock(difficulty);
 
-        System.out.println("\n Blockchain is valid: " + isChainValid());
+        // System.out.println("\n Blockchain is valid: " + isChainValid());
 
-        String bchain_json = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        // String bchain_json = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 
-        String blockchain_json = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println("Block Chain: ");
-        System.out.println(bchain_json);
+        // String blockchain_json = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        // System.out.println("Block Chain: ");
+        // System.out.println(bchain_json);
+
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        wallet_a = new Wallet();
+        wallet_b = new Wallet();
+
+        System.out.println("Private and public keys are:");
+        System.out.println(StringUtil.getStringFromKey(wallet_a.privateKey));
+        System.out.println(StringUtil.getStringFromKey(wallet_a.publicKey));
+
+        
     }
 }
